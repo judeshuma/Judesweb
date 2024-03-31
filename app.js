e7e0e74e092f6723c59b418d0048f714a1afe6d8
Jude Shuma <148837819+judeshuma@users.noreply.github.com>
2024-03-31 14:54:52 -0500

    Add files via upload
    javascript filter / blog update

---
+++ app.js
@@ -0,0 +1,94 @@
+import 'bootstrap';
+
+// document.getElementById("typewriter").addEventListener("click", handleClick)
+
+// function handleClick() {
+//   alert("I got click.");
+// }
+
+// filterObjects("all");
+
+// function filterObjects(c) {
+//     var x, i;
+//     x = document.getElementsByClassName("post");
+//     if (c == "all") c = "";
+//     for (i = 0; i < x.length; i++) {
+//         removeClass(x[i], "show");
+//         if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+//     }
+// }
+
+// function addClass(element, name) {
+//     var i, arr1, arr2;
+//     arr1 = element.className.split(" ");
+//     arr2 = name.split(" ");
+//     for (i = 0; i < arr2.length; i++) {
+//         if (arr1.indexOf(arr2[i]) == -1) {
+//             element.className += " " + arr2[i];
+//         }
+//     }
+// }
+
+// function removeClass(element, name) {
+//     var i, arr1, arr2;
+//     arr1 = element.className.split(" ");
+//     arr2 = name.split(" ");
+//     for (i = 0; i < arr2.length; i++) {
+//         while (arr1.indexOf(arr2[i]) > -1) {
+//             arr1.splice(arr1.indexOf(arr2[i]), 1); 
+//         }
+//     }
+//     element.className = arr1.join(" ");
+// }
+
+// document.getElementById("btnn").addEventListener("click", myFunction);
+
+// function myFunction(){
+//     alert("OMG I GOT CLICKED");
+// }
+
+// filterSelection("all")
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("postBlog");
+//   if (c == "all") c = "";
+//   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+//   for (i = 0; i < x.length; i++) {
+//     RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+//   }
+// }
+
+// // Show filtered elements
+// function AddClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) {
+//       element.className += " " + arr2[i];
+//     }
+//   }
+// }
+
+// // Hide elements that are not selected
+// function RemoveClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
+// var btnContainer = document.getElementById("myButtonContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function(){
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
